cf617cf7e301e912d08f802a152d3067a14bec68
Teemu Vartiainen <teemu.vartiainen@outlook.com>
2015-11-02 15:23:37 +0200

    Add real-time editing

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-script.js
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("code").addEventListener("keyup", function (event) {
+        var text = this.value;
+        document.getElementById("target").innerHTML = text;
+    });
+    var text = document.getElementById("code").value;
+    document.getElementById("target").innerHTML = text;
+});

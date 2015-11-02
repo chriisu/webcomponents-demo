90c018499cb6f540404144f2b234ddb43d9c9219
Teemu Vartiainen <teemu.vartiainen@outlook.com>
2015-11-02 15:33:03 +0200

    Add button to add tags

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    document.getElementById("code").addEventListener("keyup", function (event) {
-        var text = this.value;
-        document.getElementById("target").innerHTML = text;
-    });
+var buttons = [
+    {id: "btn-googlemap", code: "<google-map latitude=\"37.77493\" longitude=\"-122.41942\"></google-map>"}
+]
+
+function updateCode() {
     var text = document.getElementById("code").value;
     document.getElementById("target").innerHTML = text;
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("code").addEventListener("keyup", updateCode);
+
+    for (var i in buttons) {
+        var element = document.getElementById(buttons[i].id);
+        element.hiddenCode = buttons[i].code;
+        element.addEventListener("click", function (event) {
+            document.getElementById("code").value += "\n" + this.hiddenCode;
+            updateCode();
+        });
+    }
+
+    updateCode();
 });

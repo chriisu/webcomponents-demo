71a94a68d18f1f30ab30c60384255681d700edd8
chriisu <chriisu@gmail.com>
2015-11-02 16:09:37 +0200

    faster response
    update on enter or button press

--- script.js
+++ script.js
@@ -11,22 +11,31 @@ function updateCode() {
     document.getElementById("target").innerHTML = text;
 }
 
-function callUpdate() {
+function callUpdate(event) {
+	if (event) {
+		var key = event.which || event.keyCode;	
+	    if (key === 13) { // 13 is enter
+	    	updateCode();
+	    	return;
+	    }
+    }    
     if (timeout) {
         window.clearTimeout(timeout);
     }
     timeout = window.setTimeout(updateCode, 1000);
+	
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("code").addEventListener("keyup", callUpdate);
+
 
     for (var i in buttons) {
         var element = document.getElementById(buttons[i].id);
         element.hiddenCode = buttons[i].code;
         element.addEventListener("click", function (event) {
             document.getElementById("code").value += "\n" + this.hiddenCode;
-            callUpdate();
+            callUpdate({keyCode:13});
         });
     }
 
